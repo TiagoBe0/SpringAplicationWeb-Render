9445df30fe859125abed2f1cc054ad086b8a2b43
TiagoBe0 <santiagobergamin@gmail.com>
2025-05-13 15:40:47 -0300

    fis

--- src/main/java/com/proyecto/demo/controladores/PortalControlador.java
+++ src/main/java/com/proyecto/demo/controladores/PortalControlador.java
@@ -13,12 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/portal")
 public class Controlador {
 
     @Autowired
@@ -27,46 +25,21 @@ public class Controlador {
     @Autowired
     private ZonaRepositorio zonaRepositorio;
 
-    
-    
-    @GetMapping("/render")
-    public String inde_test_render(){
-    
-    return "index_render.html";
-    }
-     @GetMapping("/render-index")
-    public String index_test(){
-    
-    return "index.html";
-    }
     @GetMapping("/")
     public String index(ModelMap modelo) {
         List<Usuario> usuariosActivos = usuarioServicio.todosLosUsuarios();
         //Recordar que utilizo el modelo,para viajar con la llave usuarios al HTML la lista usuariosactivos
         modelo.addAttribute("usuarios", usuariosActivos);
-        return "error.html";
+        return "index.html";
     }
-    
-    @GetMapping("/formularioBarra")
-    public String form(ModelMap modelo) {
-        
-        
-        return "formularioBarra.html";
-    }
-  
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USUARIO')")
+    @PreAuthorize("hasAnyRole('ROLE_USUARIO_REGISTRADO')")
     @GetMapping("/inicio")
-    public String inicio(ModelMap modelo) {
-    	
-    	List<Usuario> usuarios = usuarioServicio.todosLosUsuarios();
-    	
-    	modelo.addAttribute("usuarios", usuarios);
-    	
-        return "index_app_inicio.html";
+    public String inicio() {
+        return "inicio.html";
     }
 
-    @GetMapping("/loginUsuarioModelo")
+    @GetMapping("/login")
     public String login(@RequestParam(required = false) String error, @RequestParam(required = false) String logout, ModelMap model) {
         if (error != null) {
             model.put("error", "Usuario o clave incorrectos");
@@ -74,29 +47,31 @@ public class Controlador {
         if (logout != null) {
             model.put("logout", "Ha salido correctamente.");
         }
-        return "loginUsuario1.html";
-    }
-     @GetMapping("/loginUsuarioControlador")
-    public String loginUser() {
-        
-        return "loginUsuario1.html";
+        return "login.html";
     }
 
     @GetMapping("/registro")
-    public String registro() {
-        
-        
-        return "registroUsuario.html";
+    public String registro(ModelMap modelo) {
+        List<Zona> zonas = zonaRepositorio.findAll();
+        modelo.put("zonas", zonas);
+        return "registro.html";
     }
 
     @PostMapping("/registrar")
-    public String registrar( ModelMap modelo,MultipartFile archivo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String mail, @RequestParam String clave1, @RequestParam String clave2) {
-        System.out.println("LLEGAMOS A LOS CONTROLADORES TIOOOOOOO");
+    public String registrar(ModelMap modelo, MultipartFile archivo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String mail, @RequestParam String clave1, @RequestParam String clave2, @RequestParam String idZona) {
+
         try {
-            usuarioServicio.registrar(archivo, nombre, apellido, mail, clave1, clave2);
+            usuarioServicio.registrar(archivo, nombre, apellido, mail, clave1, clave2, idZona);
         } catch (ErrorServicio ex) {
-           
-            return "index.html";
+            List<Zona> zonas = zonaRepositorio.findAll();
+            modelo.put("zonas", zonas);
+            modelo.put("error", ex.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("apellido", apellido);
+            modelo.put("mail", mail);
+            modelo.put("clave1", clave1);
+            modelo.put("clave2", clave2);
+            return "registro.html";
         }
         modelo.put("titulo", "Bienvenido a Tinder de Mascotas");
         modelo.put("descripcion", "Tu usuario fue registrado de manera satisfactoria");
